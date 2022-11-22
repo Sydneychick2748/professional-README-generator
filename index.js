dbc11683956efe999f85bb0b90ef451831211327
ana <ana@anniecannons.com>
2022-11-21 22:24:49 -0800

    added the packages and started the code for the index.js and the markdown

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require ("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(error) => {
+error ? console.error(err) : console.log(`Your successfully at the  ${fileName}`)
+    });
+
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   
+    return inquirer.prompt(questions).then((answers) => {
+       console.log (answers)
+       return answers
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+    
+        
+}      
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+  const badge = `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  return badge;
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license.length === 0) {
+    return "";
+  } else {
+    const link = `* [License](#license) `;
+    return link;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,9 +24,58 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const template= `
+# ${data.title}
+
+## Description
+
+${data.description}
+
+
+
+## Table of Contents (Optional)
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Usage](#usage)
+* ${link}
+
+
+
+## Installation
+
+${data.installation}
+
+
+## Usage
+
+${data.usage}
+
+
+
+## Licenses
+
+This project is under the ${data.license}
+${badge}
+
+##Features
+if your project has a lot of features list them here 
+
+## How to Contribute
+if you would like others to contribute you can include the guidelines of how to do so 
+
+## Tests
+
+Go the extra mile and write test for your application . Then provide examples of how to run them 
+
+## Questions
+
+if you have any questions , contact me by ${data.email}
 
 `;
+
+return template;
 }
 
 module.exports = generateMarkdown;
